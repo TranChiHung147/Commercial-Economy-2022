60319a3e6dc54c53b4a5135f7737ecc07c1c77c6
kizakari <vanminhhuyhv@gmail.com>
2022-11-05 08:23:22 +0700

    fix

---
+++ src/data.js
@@ -0,0 +1,211 @@
+const data = {
+    products: [
+        {
+            _id:'1',
+            image: require('./img/DemoProduct/1.png'),
+            sale: '-20%',
+            name: 'Xiaomi Vacuum 2 pro',
+            label: 'Xiaomi',
+            priceOff: '19,000,000 đ',
+            priceOn: '116,900,000 đ'
+        },    
+
+        {
+            _id:'2',
+            image: require('./img/DemoProduct/2.png'),
+            sale: '-20%',
+            name: 'Life 12 Pro Max',
+            label: 'iLife',
+            priceOff: '18,000,000 đ',
+            priceOn: '17,000,000 đ'
+        },
+
+        {
+            _id:'3',
+            image: require('./img/DemoProduct/3.png'),
+            sale: '-20%',
+            name: 'Ecovas ultimate 5',
+            label: 'Deebot Ecovas',
+            priceOff: '10,000,000 đ',
+            priceOn: '8,000,000 đ'
+        },
+
+        {
+            _id:'4',
+            image: require('./img/DemoProduct/4.png'),
+            sale: '-20%',
+            name: 'Liectroux 2021 Pro edition',
+            label: 'Liectroux',
+            priceOff: '5,000,000 đ',
+            priceOn: '4,500,000 đ'
+        },
+
+        {
+            _id:'5',
+            image: require('./img/DemoProduct/5.png'),
+            sale: '-20%',
+            name: 'Life 12 Pro Max',
+            label: 'iLife',
+            priceOff: '18,000,000 đ',
+            priceOn: '17,000,000 đ'
+        }
+    ],
+    yourNeed: [
+        {
+            _id: '1',
+            name: 'Đơn giản',
+            image: require('./img/YourNeed/1.png')
+        },
+        
+        {
+            _id: '2',
+            name: 'Hiện đại',
+            image: require('./img/YourNeed/2.png')
+        },
+
+        {
+            _id: '3',
+            name: 'Cao cấp',
+            image: require('./img/YourNeed/3.png')
+        },
+
+        {
+            _id: '4',
+            name: 'Phân phối độc quyền',
+            image: require('./img/YourNeed/4.png')
+        },
+
+        {
+            _id: '5',
+            name: 'Limited Edition',
+            image: require('./img/YourNeed/5.png')
+        }
+    ],
+
+    smallMarketTop: [
+        {
+            _id:'1',
+            image: require('./img/SmallMarket/1.png'),
+            sale: '-20%',
+            name: 'iLife 12 Pro Max',
+            label: 'iLife',
+            priceOff: '18,000,000 đ',
+            priceOn: '17,000,000 đ'
+        },
+
+        {
+            _id:'2',
+            image: require('./img/SmallMarket/2.png'),
+            sale: '-20%',
+            name: 'iLife 12 Pro Max',
+            label: 'iLife',
+            priceOff: '18,000,000 đ',
+            priceOn: '17,000,000 đ'
+        },
+
+        {
+            _id:'3',
+            image: require('./img/SmallMarket/3.png'),
+            sale: '-20%',
+            name: 'iLife 12 Pro Max',
+            label: 'iLife',
+            priceOff: '18,000,000 đ',
+            priceOn: '17,000,000 đ'
+        },
+
+        {
+            _id:'4',
+            image: require('./img/SmallMarket/4.png'),
+            sale: '-20%',
+            name: 'iLife 12 Pro Max',
+            label: 'iLife',
+            priceOff: '18,000,000 đ',
+            priceOn: '17,000,000 đ'
+        },
+
+        {
+            _id:'5',
+            image: require('./img/SmallMarket/5.png'),
+            sale: '-20%',
+            name: 'iLife 12 Pro Max',
+            label: 'iLife',
+            priceOff: '18,000,000 đ',
+            priceOn: '17,000,000 đ'
+        },
+
+        {
+            _id:'6',
+            image: require('./img/SmallMarket/6.png'),
+            sale: '-20%',
+            name: 'iLife 12 Pro Max',
+            label: 'iLife',
+            priceOff: '18,000,000 đ',
+            priceOn: '17,000,000 đ'
+        },
+    ],
+
+    smallMarketBot: [
+
+        {
+            _id:'7',
+            image: require('./img/SmallMarket/7.png'),
+            sale: '-20%',
+            name: 'iLife 12 Pro Max',
+            label: 'iLife',
+            priceOff: '18,000,000 đ',
+            priceOn: '17,000,000 đ'
+        },
+
+        {
+            _id:'8',
+            image: require('./img/SmallMarket/8.png'),
+            sale: '-20%',
+            name: 'iLife 12 Pro Max',
+            label: 'iLife',
+            priceOff: '18,000,000 đ',
+            priceOn: '17,000,000 đ'
+        },
+
+        {
+            _id:'9',
+            image: require('./img/SmallMarket/9.png'),
+            sale: '-20%',
+            name: 'iLife 12 Pro Max',
+            label: 'iLife',
+            priceOff: '18,000,000 đ',
+            priceOn: '17,000,000 đ'
+        },
+
+        {
+            _id:'10',
+            image: require('./img/SmallMarket/10.png'),
+            sale: '-20%',
+            name: 'iLife 12 Pro Max',
+            label: 'iLife',
+            priceOff: '18,000,000 đ',
+            priceOn: '17,000,000 đ'
+        },
+
+        {
+            _id:'11',
+            image: require('./img/SmallMarket/11.png'),
+            sale: '-20%',
+            name: 'iLife 12 Pro Max',
+            label: 'iLife',
+            priceOff: '18,000,000 đ',
+            priceOn: '17,000,000 đ'
+        },
+
+        {
+            _id:'12',
+            image: require('./img/SmallMarket/12.png'),
+            sale: '-20%',
+            name: 'iLife 12 Pro Max',
+            label: 'iLife',
+            priceOff: '18,000,000 đ',
+            priceOn: '17,000,000 đ'
+        }
+    ]
+}
+
+export default data;
